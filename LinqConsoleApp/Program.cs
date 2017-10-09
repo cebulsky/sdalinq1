f95116c79767e5fe1eb61457a9bf10f4f9d3f785
Mateusz Cebula <cebula.mat@gmail.com>
2017-10-09 17:36:17 +0000

    1.0.1

--- LinqConsoleApp/Program.cs
+++ LinqConsoleApp/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace LinqConsoleApp
+namespace ConsoleLinqToObjects
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string path = @"C:\Windows\";
+            GetFilesNamesAndSizeWithoutLinq(path);
+            Console.ReadLine();
+        }
+        private static void GetFilesNamesAndSizeWithoutLinq(string path)
+        {
+            var directoryInfo = new DirectoryInfo(path);
+            var filesInfo = directoryInfo.GetFiles();
+            Array.Sort(filesInfo, new FileSizeComparer());
+            foreach (var fi in filesInfo)
+            {
+                Console.WriteLine($"{fi.Name,-25} size: {fi.Length,10:N0}");
+            }
+        }
+    }
+    public class FileSizeComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
         }
     }
 }
